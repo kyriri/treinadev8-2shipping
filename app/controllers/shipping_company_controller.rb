f4ed8765d03b72743fe387c363520fdfd2f78502
kyriri <git-kyriri@opayq.com>
2022-05-15 16:09:25 +0200

    Add list of Shipping Cos to its index page

--- app/controllers/shipping_company_controller.rb
+++ app/controllers/shipping_company_controller.rb
@@ -1,4 +1,5 @@
 class ShippingCompanyController < ApplicationController
   def index
+    @shipping_cos = ShippingCompany.all
   end
 end
---
+++ spec/system/shipping_company/admin_sees_list_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe 'Admin access index for Shipping Companies and' do
+  it 'sees a list of them' do
+    ShippingCompany.create!(name: 'Transportes Marília Ltda',
+                            status: 0,
+                            legal_name: 'Transportes Marília Ltda',
+                            email_domain: 'tma.com.br',
+                            cnpj: 12345678904321,
+                            billing_address: 'Av. Getúlio Vargas, 32 - Marília, SP')
+    ShippingCompany.create!(name: 'Cheirex',
+                            status: 9,
+                            legal_name: 'Transportes Federais do Brasil S.A.',
+                            email_domain: 'cheirex.com',
+                            cnpj: 12345678901234,
+                            billing_address: 'Av. das Nações Unidas, 1.532 - São Paulo, SP')
+      
+    visit shipping_company_index_path
+
+    expect(page).not_to have_text('Nenhuma transportadora cadastrada')
+    expect(page).to have_text('Cheirex')
+    expect(page).to have_text('Transportes Marília')
+  end
+
+  it 'but there are no companies to show' do
+    visit shipping_company_index_path
+
+    expect(page).to have_text('Nenhuma transportadora cadastrada')
+  end
+end
