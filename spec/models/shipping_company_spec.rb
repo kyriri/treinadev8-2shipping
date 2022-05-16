0428c11c9d47c07d3eff6fe6980f5e310f097ad4
kyriri <git-kyriri@opayq.com>
2022-05-16 12:11:09 +0200

    Validate uniqueness only for non-deleted companies

--- spec/models/shipping_company_spec.rb
+++ spec/models/shipping_company_spec.rb
@@ -22,7 +22,7 @@ RSpec.describe ShippingCompany, type: :model do
 
     it 'unique fields are repeated' do
       ShippingCompany.create!(name: 'Transportes Marília',
-                              status: 2,
+                              status: 'active',
                               legal_name: 'Transportes Marília Ltda',
                               email_domain: 'tma.com.br',
                               cnpj: 12345678904321,
@@ -50,6 +50,42 @@ RSpec.describe ShippingCompany, type: :model do
       cia.valid?
 
       expect(cia.errors.full_messages).to include('CNPJ deve ter 14 caracteres')
+    end
+  end
+
+  describe 'is valid when' do
+    it 'name is repeated from a deleted company' do
+      ShippingCompany.create!(name: 'Transportes Marília',
+                              status: 'deleted',
+                              legal_name: 'Transportes Marília Ltda',
+                              email_domain: 'tam.com.br',
+                              cnpj: 12345678904321,
+                              billing_address: 'Av. Getúlio Vargas, 32 - Marília, SP')
+    
+      cia = ShippingCompany.new(name: 'Transportes Marília',
+                                    status: 'in_registration',
+                                    legal_name: 'Transportes Marília Ltda',
+                                    email_domain: 'tam2022.com.br',
+                                    cnpj: 12345678900001,
+                                    billing_address: 'Av. do Aeroporto, 10 - Rio de Janeiro, RJ')
+      expect(cia.valid?).to be true
+    end
+
+    it 'cnpj is repeated from a deleted company' do
+      ShippingCompany.create!(name: 'Transportes Marília',
+                              status: 'deleted',
+                              legal_name: 'Transportes Marília Ltda',
+                              email_domain: 'tam.com.br',
+                              cnpj: 12345678904321,
+                              billing_address: 'Av. Getúlio Vargas, 32 - Marília, SP')
+    
+      cia = ShippingCompany.new(name: 'Transportes Cardoso',
+                                    status: 'in_registration',
+                                    legal_name: 'Transportes Cardoso Ltda',
+                                    email_domain: 'cardoso.com.br',
+                                    cnpj: 12345678904321,
+                                    billing_address: 'Av. do Aeroporto, 10 - Rio de Janeiro, RJ')
+      expect(cia.valid?).to be true
     end
   end
 end
