ea35e93a299eec37d0900cd3478f0e649a183069
kyriri <git-kyriri@opayq.com>
2022-05-16 22:21:03 +0100

    Add comment to routes file

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root 'home#index'
 
   resources :shipping_companies, only: [:index, :show, :new, :create, :edit, :update]
+  
+  # the route below goes unnested so that "fake_delete" doesn't appear on URL
   post '/shipping_companies/:id', to: 'shipping_companies#fake_delete', as: 'fake_delete_shipping_company'
 
 end
