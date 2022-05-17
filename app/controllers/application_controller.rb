43571a4646d7d64fc203f168b6f4dee53758877c
kyriri <git-kyriri@opayq.com>
2022-05-17 19:35:52 +0100

    Allow dynamic change of languages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  around_action :switch_locale
+
+  def switch_locale(&action)
+    if params[:locale].nil?
+      locale = I18n.default_locale
+    else
+      locale = params[:locale].match(/[a-zA-Z-]{2,}/).to_s.to_sym
+      if I18n.available_locales.exclude?(locale)
+        locale = :en
+        flash[:notice] = 'The requested language is not available. You were redirected to the English version of the website.'
+      end
+    end
+    # locale = current_user.try(:locale) || I18n.default_locale # persists choice for logged users
+    I18n.with_locale(locale, &action)
+  end
 end
---
+++ spec/system/user_switches_languages_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe 'User requests page' do
+  it 'and by default it is in Portuguese' do
+    visit root_path
+
+    expect(page).to have_css('nav', text: 'Departamento de Envios')
+    expect(page).not_to have_text('The requested language is not available. You were redirected to the English version of the website.')
+  end
+
+  it 'in Portuguese and sees it in Portuguese' do
+    visit '/?locale=pt-BR'
+
+    expect(page).to have_css('nav', text: 'Departamento de Envios')
+  end
+
+  it 'in English and sees it in English' do
+    visit '/?locale=en'
+
+    expect(page).to have_css('nav', text: 'Shipping Department')
+  end
+
+  it 'in English even if there is a trailing space' do
+    visit '/?locale=en '
+
+    expect(page).to have_css('nav', text: 'Shipping Department')
+  end
+
+  it 'in an unsupported language and sees it in English' do
+    visit '/?locale=th'
+
+    expect(page).to have_css('nav', text: 'Shipping Department')
+    expect(page).to have_text('The requested language is not available. You were redirected to the English version of the website.')
+  end
+end
