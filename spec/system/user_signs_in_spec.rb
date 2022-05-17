362635de54e6568d97a6f4e0c9e229938ea063a9
kyriri <git-kyriri@opayq.com>
2022-05-17 20:22:21 +0100

    Create log-in page

--- spec/system/user_signs_in_spec.rb
+++ spec/system/user_signs_in_spec.rb
@@ -6,12 +6,35 @@ describe 'User attempts sign-in' do
     
     visit root_path
     click_on 'Entrar'
-    fill_in 'Email', with: 'me@email.com'
-    fill_in 'Senha', with: '12345678'
-    click_on 'Entrar'
+    within 'form' do
+      fill_in 'Email', with: 'me@email.com'
+      fill_in 'Senha', with: '12345678'
+      click_on 'Entrar'
+    end
 
-    expect(page).to have_link('Sair')
+    expect(page).to have_text('Login efetuado com sucesso')
+    expect(page).to have_css('nav', text: 'me@email.com')
     expect(page).not_to have_link('Entrar')
-    expect(page).to have_css(nav, text: 'me@email.com')
+    expect(page).to have_link('Sair')
+  end
+end
+
+describe 'User signs out' do
+  xit 'succesfully' do
+    User.create!(email: 'me@email.com', password: '12345678')
+    
+    visit root_path
+    click_on 'Entrar'
+    within 'form' do
+      fill_in 'Email', with: 'me@email.com'
+      fill_in 'Senha', with: '12345678'
+      click_on 'Entrar'
+    end
+    click_on 'Sair'
+
+    expect(page).to have_text('Logout efetuado com sucesso')
+    expect(page).not_to have_css('nav', text: 'me@email.com')
+    expect(page).to have_link('Entrar')
+    expect(page).not_to have_link('Sair')
   end
 end
