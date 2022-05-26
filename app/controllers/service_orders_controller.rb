a5f7696ded412ea47498de1553652ce87429a316
kyriri <git-kyriri@opayq.com>
2022-05-26 11:12:44 -0300

    Refactor Service Orders controller

--- app/controllers/service_orders_controller.rb
+++ app/controllers/service_orders_controller.rb
@@ -1,25 +1,6 @@
 class ServiceOrdersController < ApplicationController
   def index
-    if current_user.admin?
-      @building_blocks = [{ title: 'Novas',
-                            css_class: 'unassigned',
-                            collection: ServiceOrder.where(status: 'unassigned'),
-                          }, {
-                            title: 'Devolvidas',
-                            css_class: 'rejected',
-                            collection: ServiceOrder.where(status: 'rejected'),
-                          }]
-    else
-      company = ShippingCompany.find(current_user.shipping_company.id)
-      @building_blocks = [{ title: 'Novas',
-                            css_class: 'pending',
-                            collection: ServiceOrder.where(shipping_company_id: company.id).pending,
-                          }, {
-                            title: 'Em processo de entrega',
-                            css_class: 'accepted',
-                            collection: ServiceOrder.where(shipping_company_id: company.id).accepted,
-                          }]
-    end
+    @building_blocks = get_building_blocks
   end
 
   def show
@@ -27,5 +8,30 @@ class ServiceOrdersController < ApplicationController
     @package = @service_order.package
     @measures = [@package.width_in_cm, @package.length_in_cm, @package.height_in_cm].sort.reverse
     @quotes = @service_order.get_quotes
+  end
+
+  private
+
+  def get_building_blocks
+    if current_user.admin?
+      [{ title: 'Novas',
+        css_class: 'unassigned',
+        collection: ServiceOrder.unassigned,
+      }, {
+        title: 'Devolvidas',
+        css_class: 'rejected',
+        collection: ServiceOrder.rejected,
+      }]
+    else
+      company = ShippingCompany.find(current_user.shipping_company.id)
+      [{ title: 'Novas',
+        css_class: 'pending',
+        collection: ServiceOrder.where(shipping_company_id: company.id).pending,
+      }, {
+        title: 'Em processo de entrega',
+        css_class: 'accepted',
+        collection: ServiceOrder.where(shipping_company_id: company.id).accepted,
+      }]
+    end
   end
 end
