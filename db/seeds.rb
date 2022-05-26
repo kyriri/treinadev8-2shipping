a0e5872da91322faa6b79e4a07d4bec825f90f3b
kyriri <git-kyriri@opayq.com>
2022-05-26 13:45:47 -0300

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,17 +7,17 @@ Package.destroy_all
 ShippingCompany.destroy_all
 User.destroy_all
 
-sc1 = ShippingCompany.create!(name: 'Transportes Marília',
-                              status: 'deleted',
-                              legal_name: 'Transportes Marília Ltda',
-                              email_domain: 'tma.com.br',
-                              cnpj: 12345678904321,
-                              billing_address: 'Av. Getúlio Vargas, 32 - Marília, SP',
+sc1 = ShippingCompany.create!(status: 'active',
+                              name: 'Air Cargo',
+                              legal_name: 'Empresa Brasileira de Transporte de Cargas Ltda',
+                              email_domain: 'aircargo.com.br',
+                              cnpj: 54321678900011,
+                              billing_address: 'Av. Atlântica, Rio de Janeiro - RJ',
                               cubic_weight_const: 600,
                               min_fee: 45
                              )
-sc2 = ShippingCompany.create!(name: 'Cheirex',
-                              status: 'active',
+sc2 = ShippingCompany.create!(status: 'active',
+                              name: 'Cheirex',
                               legal_name: 'Transportes Federais do Brasil S.A.',
                               email_domain: 'cheirex.com',
                               cnpj: 12345678901234,
@@ -25,8 +25,8 @@ sc2 = ShippingCompany.create!(name: 'Cheirex',
                               cubic_weight_const: 35,
                               min_fee: 8,
                              )
-sc3 = ShippingCompany.create!(name: 'Ibérica',
-                              status: 'active',
+sc3 = ShippingCompany.create!(status: 'active',
+                              name: 'Ibérica',
                               legal_name: 'Ibérica dos Transportes Ltda',
                               email_domain: 'iberica.com.br',
                               cnpj: 98765432101234,
@@ -34,8 +34,8 @@ sc3 = ShippingCompany.create!(name: 'Ibérica',
                               cubic_weight_const: 30,
                               min_fee: 10,
                              )
-sc4 = ShippingCompany.create!(name: 'Granelero',
-                              status: 'in_registration',
+sc4 = ShippingCompany.create!(status: 'in_registration', 
+                              name: 'Granelero',
                               legal_name: 'Transportes Graneleros do Brasil S.A.',
                               email_domain: 'br.granelero.com',
                               cnpj: 34259054000134,
@@ -43,48 +43,61 @@ sc4 = ShippingCompany.create!(name: 'Granelero',
                               cubic_weight_const: 20,
                               min_fee: 10,
                              )
-package1 = Package.create!(width_in_cm: 14,
-                            height_in_cm: 6,
-                            length_in_cm: 21,
-                            volume_in_m3: Float(14 * 6 * 21)/1_000_000,
-                            weight_in_g: 250,
-                            distance_in_km: 800,
-                            pickup_address: 'Rodovia Dutra km3, Guarulhos - SP',
-                            delivery_address: 'Av. do Contorno 354 - Belo Horizonte, MG',
-                            delivery_recipient_name: 'José Magela Amorim',
-                            delivery_recipient_phone: '(31) 9 9453-8890',
-                          )
-package2 = Package.create!(width_in_cm: 100,
-                            height_in_cm: 86,
-                            length_in_cm: 35,
-                            volume_in_m3: Float(100 * 86 * 35)/1_000_000,
-                            weight_in_g: 1_250,
-                            distance_in_km: 12,
-                            pickup_address: 'Av. Interlagos, 2.350',
-                            delivery_address: 'Av. Corifeu de Azevedo Marques, 251 / 81',
-                            delivery_recipient_name: 'Carla Callegari',
-                            delivery_recipient_phone: '(11) 9 8431-9106',
-                          )
-package3 = Package.create!(width_in_cm: 10,
-                            height_in_cm: 4,
-                            length_in_cm: 22,
-                            volume_in_m3: Float(10 * 4 * 22)/1_000_000,
-                            weight_in_g: 182,
-                            distance_in_km: 12,
-                            pickup_address: 'Av. Interlagos, 2.350',
-                            delivery_address: 'Av. Corifeu de Azevedo Marques, 251 / 81',
-                            delivery_recipient_name: 'Carla Callegari',
-                            delivery_recipient_phone: '(11) 9 8431-9106',
-                          )
+sc5 = ShippingCompany.create!(status: 'deleted',
+                              name: 'Transportes Marília',
+                              legal_name: 'Transportes Marília Ltda',
+                              email_domain: 'tma.com.br',
+                              cnpj: 12345678904321,
+                              billing_address: 'Av. Getúlio Vargas, 32 - Marília, SP',
+                             )
+complete_package1 = Package.create!(width_in_cm: 14,
+                                    height_in_cm: 6,
+                                    length_in_cm: 21,
+                                    volume_in_m3: Float(14 * 6 * 21)/1_000_000,
+                                    weight_in_g: 250,
+                                    distance_in_km: 800,
+                                    pickup_address: 'Galpão TFD - Rodovia Dutra km3, Guarulhos - SP',
+                                    delivery_address: 'Av. do Contorno 354 - Belo Horizonte, MG',
+                                    delivery_recipient_name: 'José Magela Amorim',
+                                    delivery_recipient_phone: '(31) 9 9453-8890',
+                                  )
+complete_package2 = Package.create!(width_in_cm: 100,
+                                    height_in_cm: 86,
+                                    length_in_cm: 35,
+                                    volume_in_m3: Float(100 * 86 * 35)/1_000_000,
+                                    weight_in_g: 1_250,
+                                    distance_in_km: 12,
+                                    pickup_address: 'Av. Interlagos, 2.350',
+                                    delivery_address: 'Av. Corifeu de Azevedo Marques, 251 / 81',
+                                    delivery_recipient_name: 'Carla Callegari',
+                                    delivery_recipient_phone: '(11) 9 8431-9106',
+                                  )
+complete_package3 = Package.create!(width_in_cm: 10,
+                                    height_in_cm: 4,
+                                    length_in_cm: 22,
+                                    volume_in_m3: Float(10 * 4 * 22)/1_000_000,
+                                    weight_in_g: 182,
+                                    distance_in_km: 12,
+                                    pickup_address: 'Av. Interlagos, 2.350',
+                                    delivery_address: 'Av. Corifeu de Azevedo Marques, 251 / 81',
+                                    delivery_recipient_name: 'Carla Callegari',
+                                    delivery_recipient_phone: '(11) 9 8431-9106',
+                                  )
+simple_package1 = Package.create!(volume_in_m3: 0.0034, weight_in_g: 182, distance_in_km: 12)
+simple_package2 = Package.create!(volume_in_m3: 0.0017, weight_in_g: 500, distance_in_km: 50)
+simple_package3 = Package.create!(volume_in_m3: 0.001, weight_in_g: 20, distance_in_km: 273)
+simple_package4 = Package.create!(volume_in_m3: 0.004, weight_in_g: 1750, distance_in_km: 800)
+simple_package5 = Package.create!(volume_in_m3: 0.0105, weight_in_g: 3276, distance_in_km: 39)
 
                 
-ServiceOrder.create!(status: 'canceled', package: Package.new)
-ServiceOrder.create!(status: 'unassigned', package: package1)
-ServiceOrder.create!(status: 'unassigned', package: package2)
-ServiceOrder.create!(status: 'rejected', package: package3)
-ServiceOrder.create!(status: 'pending', package: Package.new)
-ServiceOrder.create!(status: 'accepted', package: Package.new)
-ServiceOrder.create!(status: 'delivered', package: Package.new)
+ServiceOrder.create!(status: 'unassigned', package: simple_package1)
+ServiceOrder.create!(status: 'unassigned', package: simple_package2)
+ServiceOrder.create!(status: 'rejected', package: simple_package3, shipping_company: sc2)
+ServiceOrder.create!(status: 'pending', package: complete_package1, shipping_company: sc2)
+ServiceOrder.create!(status: 'pending', package: complete_package2, shipping_company: sc2)
+ServiceOrder.create!(status: 'accepted', package: complete_package3, shipping_company: sc2)
+ServiceOrder.create!(status: 'canceled', package: simple_package4)
+ServiceOrder.create!(status: 'delivered', package: simple_package5)
 
 ShippingRate.create!(max_weight_in_kg: 0.3, cost_per_km_in_cents: 50, shipping_company: sc3)
 ShippingRate.create!(max_weight_in_kg: 0.5, cost_per_km_in_cents: 55, shipping_company: sc3)
