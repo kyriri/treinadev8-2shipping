90f5ad20faad6859b15ccf8090c30eb322d97cec
kyriri <git-kyriri@opayq.com>
2022-05-25 21:00:27 -0300

    Add role of admin to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_24_012153) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_25_235914) do
   create_table "delivery_times", force: :cascade do |t|
     t.integer "max_distance_in_km"
     t.integer "delivery_time_in_buss_days"
@@ -74,6 +74,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_24_012153) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
