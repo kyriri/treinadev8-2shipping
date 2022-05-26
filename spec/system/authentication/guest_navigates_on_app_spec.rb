5972ff2e88647895c7812985ec91b1ea783d8f1a
kyriri <git-kyriri@opayq.com>
2022-05-26 14:36:56 -0300

    Update test

--- spec/system/authentication/guest_navigates_on_app_spec.rb
+++ spec/system/authentication/guest_navigates_on_app_spec.rb
@@ -8,7 +8,8 @@ describe 'Unlogged user visits the application and' do
   end
 
   it 'is redirected to the desired page after login' do
-    User.create!(email: 'me@email.com', password: '12345678')
+    sc1 = ShippingCompany.create!(status: 'active', name: 'Cheirex', legal_name: 'Transportes Federais do Brasil S.A.', email_domain: 'cheirex.com', cnpj: 12345678901234, billing_address: 'Av. das Nações Unidas, 1.532 - São Paulo, SP', cubic_weight_const: 35, min_fee: 8)
+    User.create!(email: 'me@email.com', password: '12345678', shipping_company: sc1)
 
     visit service_orders_path
     fill_in 'Email', with: 'me@email.com'
