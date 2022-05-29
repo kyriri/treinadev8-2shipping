80b644a3618bbb5532f9e1b9adf1c81cf70b26b6
kyriri <git-kyriri@opayq.com>
2022-05-29 18:53:35 -0300

    Block repeated Outposts from being saved by a User

--- app/models/outpost.rb
+++ app/models/outpost.rb
@@ -2,4 +2,13 @@ class Outpost < ApplicationRecord
   belongs_to :shipping_company
   has_many :stages
   has_many :deliveries, through: :stages
+
+  validates_presence_of :name, :city_state, :category, unless: -> { standard }
+  validate :uniqueness_of_outpost
+
+  def uniqueness_of_outpost
+    if Outpost.where(name: self.name, city_state: self.city_state, category: self.category, shipping_company: self.shipping_company).present?
+      errors.add(:outpost_, I18n.t('activerecord.errors.models.outpost.not_unique'))
+    end
+  end
 end
--- spec/models/outpost_spec.rb
+++ spec/models/outpost_spec.rb
@@ -1,22 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Outpost, type: :model do
-  xdescribe '#valid?' do
-  # TODO I need custom validations to implement this
+  describe '#valid?' do
+    it 'is false if fields are blank' do
+      outpost = Outpost.new()
+
+      outpost.valid?
+      
+      expect(outpost.errors.full_messages).to include('Nome não pode ficar em branco')
+      expect(outpost.errors.full_messages).to include('Localidade não pode ficar em branco')
+      expect(outpost.errors.full_messages).to include('Categoria não pode ficar em branco')
+    end
+
+    it 'is true if standard outposts have blank fields' do
+      sc1 = ShippingCompany.create!(status: 'active', name: 'Cheirex', legal_name: 'Transportes Federais do Brasil S.A.', email_domain: 'cheirex.com', cnpj: 12345678901234, billing_address: 'Av. das Nações Unidas, 1.532 - São Paulo, SP', cubic_weight_const: 35, min_fee: 8)
+      outpost = Outpost.new(standard: true, shipping_company: sc1)
+
+      response = outpost.valid?
+      
+      expect(response).to be true
+    end
+
     it 'is false when outposts are repeated' do
       sc1 = ShippingCompany.create!(status: 'active', name: 'Cheirex', legal_name: 'Transportes Federais do Brasil S.A.', email_domain: 'cheirex.com', cnpj: 12345678901234, billing_address: 'Av. das Nações Unidas, 1.532 - São Paulo, SP', cubic_weight_const: 35, min_fee: 8)
-      Outpost.create!(shipping_company: sc1, name: 'Zona Sul', city_state: 'São Paulo, SP', category: 'centro de distribuição')
+                Outpost.create!(shipping_company: sc1, name: 'Zona Sul', city_state: 'São Paulo, SP', category: 'centro de distribuição')
       new_outpost = Outpost.new(shipping_company: sc1, name: 'Zona Sul', city_state: 'São Paulo, SP', category: 'centro de distribuição')
       
       new_outpost.valid?
 
-      expect(new_outpost.errors.full_messages).to include('já')
+      expect(new_outpost.errors.full_messages).to include('Entreposto já existe')
     end
     
-    it 'is true if the repeated outpost was soft deleted' do
+    xit 'is true if the repeated outpost was soft deleted' do
       sc1 = ShippingCompany.create!(status: 'active', name: 'Cheirex', legal_name: 'Transportes Federais do Brasil S.A.', email_domain: 'cheirex.com', cnpj: 12345678901234, billing_address: 'Av. das Nações Unidas, 1.532 - São Paulo, SP', cubic_weight_const: 35, min_fee: 8)
       Outpost.create!(deleted: true, shipping_company: sc1, name: 'Zona Sul', city_state: 'São Paulo, SP', category: 'centro de distribuição')
-      new_outpost = Outpost.new(shipping_company: sc1, name: 'Zona Sul', city_state: 'São Paulo, SP', category: 'centro de distribuição')
+           new_outpost = Outpost.new(shipping_company: sc1, name: 'Zona Sul', city_state: 'São Paulo, SP', category: 'centro de distribuição')
       
       response = new_outpost.valid?
 
