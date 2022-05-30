a8449d99b4f37058b6c127db65b57e61a54aabd3
kyriri <git-kyriri@opayq.com>
2022-05-30 18:24:45 -0300

    Simplify code home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
-    @number_unassigned_packages = ServiceOrder.where(status: 'unassigned').count
-    @number_pending_packages = ServiceOrder.where(status: 'pending').count
-    @number_rejected_packages = ServiceOrder.where(status: 'rejected').count
+    @number_unassigned_packages = ServiceOrder.unassigned.count
+    @number_pending_packages = ServiceOrder.pending.count
+    @number_rejected_packages = ServiceOrder.rejected.count
   end
 end
